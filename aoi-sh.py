daaff4d89f81e558f006f2fe1e4792e9891827af
aoi-stella <halcorder@gmail.com>
2024-01-05 22:59:59 +0900

    リファクタ実施

--- aoi-sh.py
+++ aoi-sh.py
@@ -1,20 +1,53 @@
+import os
 from pwn import *
 
-print("↓攻撃対象のバイナリファイルを指定してください↓")
-dst_bin_path = input()
+def check_bin_path(bin_path: str) -> bool:
+    """バイナリファイルのパスの正当性チェック
 
-proc = process(dst_bin_path)
+    Args:
+        bin_path (str): バイナリファイルのパス
 
-pattern_length = 1000
-pattern = cyclic(pattern_length)
+    Returns:
+        bool: バイナリファイルのパスが正しいかどうか
+    """
+    if bin_path == "":
+        return False
+    return os.path.isfile(bin_path)
 
-print("以下のパターンを使用してプログラムをGDBで実行し、クラッシュさせてください:")
-print(pattern.decode('utf-8' + "\n"))
+def calc_offset_for_rip(rip_value: str) -> (int, bool):
+    """クラッシュ時のRIP値からオフセットを計算する
 
-rip_value = input("クラッシュ時のRBP値: ")
+    Args:
+        rip_value (str): クラッシュ時のRIP値
 
-# 入力された値を16進数に変換
-rip_value = int(rip_value, 16)
+    Returns:
+        int: オフセット
+    """
+    # 入力された値を16進数に変換
+    rip_value = int(rip_value, 16)
 
-offset = cyclic_find(rip_value)
-print(f"計算されたオフセット: {offset}")
+    offset = cyclic_find(rip_value)
+    if offset <= 0:
+        return offset, False
+    return offset, True
+
+if __name__ == "__main__":
+    dst_bin_path = input("Specify destination binary file path: ")
+    if not check_bin_path(dst_bin_path):
+        print("specified path is invalid...")
+        exit()
+
+    proc = process(dst_bin_path)
+
+    pattern_length = 1000
+    pattern = cyclic(pattern_length)
+
+    print("You copy this pattern and execute destination program")
+    print(pattern.decode('utf-8' ))
+    rip_value = input("When crashed RBP address: ")
+    offset, result = calc_offset_for_rip(rip_value)
+    if not result:
+        print("Failed to calc offset...")
+        exit()
+    print("Succeed to calc offset!!")
+    print(f"Offset for rip registers: {offset}")
